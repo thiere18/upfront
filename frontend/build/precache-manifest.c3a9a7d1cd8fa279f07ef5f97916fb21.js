518b37b375dec56a44fa5cbaef23339375e82b97
thierno <thiernoibrahimalo@esp.sn>
2021-12-22 21:06:50 +0000

    fixing some bugs

---
+++ frontend/build/precache-manifest.c3a9a7d1cd8fa279f07ef5f97916fb21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6d194f76bafe19cdd55a6afcbb0df9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d4a74dfeee0b1f2194e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fdcc693431ee5d71927a",
+    "url": "/static/js/2.8f110eff.chunk.js"
+  },
+  {
+    "revision": "e66d5f62cddbd9f14d019408603ab562",
+    "url": "/static/js/2.8f110eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4a74dfeee0b1f2194e",
+    "url": "/static/js/main.fc950628.chunk.js"
+  },
+  {
+    "revision": "934c498a4ec70f8f5d36",
+    "url": "/static/js/runtime-main.e2ec6b1c.js"
+  }
+]);
